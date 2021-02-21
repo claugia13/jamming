73c1df2e74fae3667577737cf14792e09f4c809e
claugia13 <ac.delima13@gmail.com>
2021-02-20 19:13:08 -0700

    add render method (step 11)

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Codecademy!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+  <h1>Ja<span className="highlight">mmm</span>ing</h1>
+  <div className="App">
+    <SearchBar />
+    <div className="App-playlist">
+      <SearchResults />
+      <Playlist />
     </div>
-  );
+  </div>
+</div>
+    )
+  }
 }
 
 export default App;
