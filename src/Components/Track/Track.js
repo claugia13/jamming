450ad2c9579f4c6b865af784b385053148c6df5c
claugia13 <ac.delima13@gmail.com>
2021-02-21 12:43:39 -0700

    correct class to className, replace hard code

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -15,11 +15,11 @@ class Track extends React.Component {
     render() {
         return(
             <div className="Track">
-  <div class="Track-information">
+  <div className="Track-information">
     <h3><!-- track name will go here --></h3>
     <p><!-- track artist will go here--> | <!-- track album will go here --></p>
   </div>
-  <button className="Track-action"><!-- + or - will go here --></button>
+  {this.renderAction()} 
 </div>
         )
     }
