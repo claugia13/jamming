63bffb6406ea270b9b2faaa150d4ffe7448642f6
claugia13 <ac.delima13@gmail.com>
2021-02-24 13:53:09 -0700

    add post request to create new playlist

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -69,6 +69,11 @@ getAccessToken(){
              }).then(response => response.json()
              ).then(jsonResponse => {
                  const playlistId = jsonResponse.id;
+                 return fetch(`/v1/users/${userId}/playlists/${playlistId}/tracks`, {
+                     headers: headers,
+                     method: 'POST',
+                     body: JSON.stringify({ uris: trackUris})
+                 })
              })
          })  
     }
