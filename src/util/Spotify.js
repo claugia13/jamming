6e6cdb9b1675735f0ef99f28ebc16a837e51ae05
claugia13 <ac.delima13@gmail.com>
2021-02-24 12:44:40 -0700

    add .then() method

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -32,6 +32,21 @@ getAccessToken(){
         return fetch(`https://api.spotify.com/v1/search?type=track&q=${searchTerm}`, { headers: {
             Authorization: `Bearer ${accessToken}`
         }
+    }) .then(response => {
+        return response.json();
+    }) .then(jsonResponse => {
+        if(!jsonResponse.tracks) {
+            return [];
+        }
+        else {
+            return jsonResponse.tracks.items.map(track => ({
+                id: track.id,
+                name: track.name,
+                artist: track.artists[0].name,
+                album: track.album.name,
+                uri: track.uri
+            }));
+        }
     });
     }
 
