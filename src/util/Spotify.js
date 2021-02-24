2e9a6fc8e47be278edb63d8e8852250065ebf194
claugia13 <ac.delima13@gmail.com>
2021-02-24 13:24:10 -0700

    add fetch request to return spotify username

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -54,10 +54,15 @@ getAccessToken(){
         if(!name || !trackUris.length) {
             return;
         }
-        
+
          const accessToken = Spotify.getAccessToken();
          const headers = { Authorization: `Bearer ${accessToken}`};
          let userId;
+
+         return fetch('https://api.spotify.com/v1/me', { headers: headers}).then(response => response.json()
+         ).then(jsonResponse => {
+             userId = jsonResponse.id;
+         })  
     }
 
 }
