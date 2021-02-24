e2db7f2d0892a3192667533c6eb2f12045a4bca5
claugia13 <ac.delima13@gmail.com>
2021-02-24 09:31:44 -0700

    create method getAccessToken

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,5 +1,16 @@
-const Spotify = {
+let userAccessToken;
 
+const Spotify = {
+getAccessToken(){
+    if(userAccessToken) {
+        return userAccessToken;
+    }
+    else {
+        // check for access token match
+        const userAccessToken = window.location.href.match(access_token=([^&]*));
+        const expiresInMatch = window.location.href.match(expires_in=([^&]*));
+    }
+}
 }
 
 export default Spotify;
